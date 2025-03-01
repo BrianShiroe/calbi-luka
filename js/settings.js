0fd16c0f05abc1742f4bf13a7a8defe934e5dba2
LCbalsa <coldwaterclean@gmail.com>
2025-03-02 02:21:25 +0800

    settings

---
+++ js/settings.js
@@ -0,0 +1,8 @@
+document.querySelectorAll(".tab").forEach(tab => {
+    tab.addEventListener("change", function() {
+        document.querySelectorAll(".settings-panel").forEach(panel => panel.style.display = "none");
+        if (this.id === "tab1") document.getElementById("general-settings").style.display = "block";
+        if (this.id === "tab2") document.getElementById("account-settings").style.display = "block";
+        if (this.id === "tab3") document.getElementById("security-settings").style.display = "block";
+    });
+});
