3ddc11033017bf51447569116cbe2980de8e09fc
LCbalsa <coldwaterclean@gmail.com>
2025-03-29 18:32:13 +0800

    remove collapse

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,28 +3,28 @@ document.addEventListener("DOMContentLoaded", () => {
     let toggleBtn = document.querySelector("#btn");
     let body = document.body;
 
-    if (isSidebarClosed) {
-        sidebar.classList.add("closed");
-    }
+    // if (isSidebarClosed) {
+    //     sidebar.classList.add("closed");
+    // }
 
     function updateBodyPadding() {
         body.style.paddingLeft = sidebar.classList.contains("closed") ? "100px" : "220px";
     }
 
-    function toggleSidebar() {
-        let isClosed = sidebar.classList.toggle("closed");
-        body.classList.toggle("sidebar-closed", isClosed);
-        updateBodyPadding();
-        updateButtonIcon(isClosed);
-        localStorage.setItem("sidebarClosed", isClosed);
-    }
+    // function toggleSidebar() {
+    //     let isClosed = sidebar.classList.toggle("closed");
+    //     body.classList.toggle("sidebar-closed", isClosed);
+    //     updateBodyPadding();
+    //     // updateButtonIcon(isClosed);
+    //     localStorage.setItem("sidebarClosed", isClosed);
+    // }
 
-    function updateButtonIcon(isClosed) {
-        toggleBtn.classList.replace(isClosed ? "bx-chevron-left" : "bx-chevron-right", isClosed ? "bx-chevron-right" : "bx-chevron-left");
-    }
+    // function updateButtonIcon(isClosed) {
+    //     toggleBtn.classList.replace(isClosed ? "bx-chevron-left" : "bx-chevron-right", isClosed ? "bx-chevron-right" : "bx-chevron-left");
+    // }
 
     updateBodyPadding();
-    updateButtonIcon(isSidebarClosed);
+    // updateButtonIcon(isSidebarClosed);
     toggleBtn.addEventListener("click", toggleSidebar);
 });
 
