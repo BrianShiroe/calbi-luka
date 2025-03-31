6a9bb04887917d9d044c577f7d993e1a3450b537
Brian Haw <brianshiroe@gmail.com>
2025-03-31 12:27:44 +0800

    playback basic design

--- js/playback.js
+++ js/playback.js
@@ -7,9 +7,13 @@ document.addEventListener("DOMContentLoaded", async function () {
         container.innerHTML = ""; // Clear existing videos
 
         videoUrls.forEach((url) => {
+            let videoCard = document.createElement("div");
+            videoCard.classList.add("video-card");
+
             let videoElement = document.createElement("video");
-            videoElement.width = 320;
-            videoElement.height = 240;
+            videoElement.style.width = "55vh";
+            videoElement.style.height = "30vh";
+            videoElement.style.borderRadius = "8px";
             videoElement.controls = true;
 
             let source = document.createElement("source");
@@ -17,7 +21,8 @@ document.addEventListener("DOMContentLoaded", async function () {
             source.type = "video/mp4";
 
             videoElement.appendChild(source);
-            container.appendChild(videoElement);
+            videoCard.appendChild(videoElement);
+            container.appendChild(videoCard);
         });
     } catch (error) {
         console.error("Error loading videos:", error);
